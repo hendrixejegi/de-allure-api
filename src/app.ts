a0104394288743c5583873ac220504da69b55415
Hendrix Ejegi <tosanwumiae@gmail.com>
2026-01-04 02:16:08 +0100

    initialize betterauth and database

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import { errorHandler } from './middlewares/error-handler';
 import config from './config/config';
+import { toNodeHandler } from 'better-auth/node';
+import { auth } from './lib/auth';
 
 const app = express();
+
+app.all('/api/auth/*splat', toNodeHandler(auth));
 
 app.use(express.json());
 
